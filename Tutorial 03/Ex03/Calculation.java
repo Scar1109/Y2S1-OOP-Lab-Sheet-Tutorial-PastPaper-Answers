c73f632596ed6a6d4e6614ee434e995344ea3603
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2023-08-25 17:29:27 +0530

    Add files via upload

---
+++ Tutorial 03/Ex03/Calculation.java
@@ -0,0 +1,10 @@
+public class Calculation {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
---
+++ Tutorial 03/Ex03/DemoApp.java
@@ -0,0 +1,7 @@
+public class DemoApp {
+    public static void main(String[] args) {
+
+        System.out.println("Addition result: " + Calculation.add(10, 5));
+        System.out.println("Subtraction result: " + Calculation.subtract(20, 8));
+    }
+}
